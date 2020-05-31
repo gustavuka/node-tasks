9a850d214b51aec96a988dc12398919f322d719b
gustavuka <go.gusfm@gmail.com>
2020-05-31 19:14:21 -0300

    feat(deploy): start setting up deploy

--- src/emails/account.js
+++ src/emails/account.js
@@ -2,12 +2,12 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const welcomeEmail = (email, name) => {
+const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
         from: 'gustavo_fm2@hotmail.com',
         subject: 'Thanks for joining our tasks platform!',
-        text: `Welcome to our app ${name}, if you need any further assistence please contact me.`
+        text: `Welcome to our app ${name}, if you need any further assistence please contact me.`,
     })
 }
 
@@ -16,8 +16,8 @@ const sendCancelationEmail = (email, name) => {
         to: email,
         from: 'gustavo_fm2@hotmail.com',
         subject: 'We are sad to see you go :(',
-        text: `${name}, We would like to know why you are leaving... Please let us know.`
+        text: `${name}, We would like to know why you are leaving... Please let us know.`,
     })
 }
 
-module.exports = { welcomeEmail, sendCancelationEmail }
+module.exports = { sendWelcomeEmail, sendCancelationEmail }
